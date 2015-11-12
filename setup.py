ee27a8c6f5489bdebecc25bf6632dc03e10dd962
pascal-schetelat <pascal.schetelat@gmail.com>
2015-11-12 17:21:06 +0100

    Fixing new setuptools issue with python setup.py test in python3

--- setup.py
+++ setup.py
@@ -1,20 +1,25 @@
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
 import sys
+from setuptools.command.test import test as TestCommand
 
 
 class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
     def finalize_options(self):
         TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
+        #self.test_args = []
+        #self.test_suite = True
 
     def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
         import pytest
-
-        errcode = pytest.main(self.test_args)
-        sys.exit(errcode)
-
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 exec(open('version.py').read())
 
